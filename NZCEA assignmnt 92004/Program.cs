110d7bb7a978c8f2175f96c6bacc8f160388ced1
grdri <grdri@TOMATOSAUDE123>
2026-03-24 16:30:30 +1300

    Add project files.

---
+++ NZCEA assignmnt 92004/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NZCEA_assignmnt_92004
+{
+    internal class Program
+    {
+        //global variables
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Insurance calculator");
+        }
+    }
+}
